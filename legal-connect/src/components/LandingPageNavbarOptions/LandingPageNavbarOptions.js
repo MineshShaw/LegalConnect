9b352199134515ce96f439d73d67b867d5f201fd
MineshShaw <mineshshaw2007@gmail.com>
2025-02-13 19:46:57 +0530

    basic landing page header

---
+++ legal-connect/src/components/LandingPageNavbarOptions/LandingPageNavbarOptions.js
@@ -0,0 +1,12 @@
+import './LandingPageNavbarOptions.css';
+
+const LandingPageNavbarOptions = () => {
+    return ( 
+        <div className="links">
+            <button className="btn" href="/login">Login</button>
+            <button className="btn" href="/signup">Signup</button>
+        </div>
+     );
+}
+ 
+export default LandingPageNavbarOptions;
