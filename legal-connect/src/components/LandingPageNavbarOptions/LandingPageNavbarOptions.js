f2cf86cb43ba67dd86e35af65eab037738473560
MineshShaw <mineshshaw2007@gmail.com>
2025-02-16 15:25:12 +0530

    changes placement of nav links

--- legal-connect/src/components/LandingPageNavbarOptions/LandingPageNavbarOptions.js
+++ legal-connect/src/components/LandingPageNavbarOptions/LandingPageNavbarOptions.js
@@ -1,10 +1,13 @@
 import './LandingPageNavbarOptions.css';
+import {useNavigate} from 'react-router-dom';
 
 const LandingPageNavbarOptions = () => {
+    const navigate = useNavigate();
     return ( 
         <div className="links">
             <button className="btn" href="/login">Login</button>
             <button className="btn" href="/signup">Signup</button>
+            <button className="btn" onClick={() => navigate("/aboutus")}>About Us</button>
         </div>
      );
 }
