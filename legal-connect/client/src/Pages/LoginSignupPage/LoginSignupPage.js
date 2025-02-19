3a9ff9f56df5b8329fddce96f0a64fd518c1cabf
MineshShaw <mineshshaw2007@gmail.com>
2025-02-19 18:53:01 +0530

    changing login signup page

--- legal-connect/client/src/Pages/LoginSignupPage/LoginSignupPage.js
+++ legal-connect/client/src/Pages/LoginSignupPage/LoginSignupPage.js
@@ -1,6 +1,7 @@
 import './LoginSignupPage.css';
+import { useState } from 'react';
 
-function handleLogin(e) {
+function handleLogin(actionType, e) {
     e.preventDefault();
 
     const email = document.querySelector('#login-email').value;
@@ -11,7 +12,7 @@ function handleLogin(e) {
         return;
     }
 
-    fetch('http://localhost:8000/register', {
+    fetch('http://localhost:8000/' + actionType, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -24,22 +25,30 @@ function handleLogin(e) {
 }
 
 const LoginSignupPage = () => {
+    const [pageTitle, setPageTitle] = useState('Login');
+    
     return ( 
         <div className="login-signup-page">
-            <h2>Login/Signup</h2>
-            <div className="container login-signup-form">
-                <form>
-                    <div className="form-group">
+            <div className="selectLoginSignup">
+                <button className="btn" onClick={() => setPageTitle('Login')}>Login</button>
+                <button className="btn" onClick={() => setPageTitle('Signup')}>Signup</button>
+            </div>
+            <div className="login-signup-container">
+                <h2>{pageTitle}</h2>
+                <div className="container login-signup-form">
+                    <form>
+                        <div className="form-group">
 
-                        <label for="login-email">Email address</label>
-                        <input type="email" className="form-control" id="login-email" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <label for="login-email">Email address</label>
+                            <input type="email" className="form-control" id="login-email" aria-describedby="emailHelp" placeholder="Enter email" />
 
-                        <label for="login-password">Password</label>
-                        <input type="password" className="form-control" id="login-password" placeholder="Password" />
+                            <label for="login-password">Password</label>
+                            <input type="password" className="form-control" id="login-password" placeholder="Password" />
 
-                        <button type="submit" className="btn" onClick={handleLogin}>Submit</button>
-                    </div>
-                </form>
+                            <button type="submit" className="btn" onClick={(e) => handleLogin(pageTitle.toLowerCase(), e)}>Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
      );
