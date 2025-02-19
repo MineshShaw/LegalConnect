f63b8be2bf283a980e4e186ccfde0269fe8a6797
MineshShaw <mineshshaw2007@gmail.com>
2025-02-19 22:00:34 +0530

    added user authentication(signin, signout, signup)

--- legal-connect/client/src/App.js
+++ legal-connect/client/src/App.js
@@ -3,15 +3,12 @@ import HomePage from './Pages/HomePage/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import LoggedOutLinks from './components/Links/LoggedOutLinks';
-import LoggedInLinks from './components/Links/LoggedInLinks';
 import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
 import LandingPage from './Pages/LandingPage/LandingPage';
-import { useState } from 'react';
 import LoginSignupPage from './Pages/LoginSignupPage/LoginSignupPage';
 import MyProfile from './Pages/MyProfile/MyProfile';
 import ExplorePage from './Pages/ExplorePage/ExplorePage';
-import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ClerkProvider} from '@clerk/clerk-react';
 
 function App() {
 
@@ -24,7 +21,7 @@ function App() {
             <Routes>
               <Route exact path="/" element={<LandingPage />} />
               <Route exact path="/explore" element={<ExplorePage />} />
-            <Route exact path="/home" element={<HomePage />} />
+              <Route exact path="/home" element={<HomePage />} />
               <Route exact path="/aboutus" element={<AboutUsPage />} />
               <Route exact path="/login" element={<LoginSignupPage />} />
               <Route exact path="/myprofile/:id" element={<MyProfile />} />
--- legal-connect/client/src/components/Navbar/Navbar.js
+++ legal-connect/client/src/components/Navbar/Navbar.js
@@ -1,21 +1,23 @@
 import './Navbar.css';
 import AppLogo from '../AppLogo/AppLogo';
-import { useAuth } from '@clerk/clerk-react';
+import { SignedIn, SignedOut} from '@clerk/clerk-react';
 import LoggedInLinks from '../Links/LoggedInLinks';
 import LoggedOutLinks from '../Links/LoggedOutLinks';
 
 const Navbar = () => {
-    const isLoggedIn = useAuth();
     return ( 
         <div className="navbar">
-            <div className="navbar-main">
+            <div className="navbar-right">
             <AppLogo/>
-            <h1>Legal Connect</h1>
-            {isLoggedIn.isSignedIn ? <LoggedInLinks /> : <LoggedOutLinks />}
             </div>
-
-          
-          
+            <div className="navbar-left">
+                <SignedIn>
+                    <LoggedInLinks />
+                </SignedIn>
+                <SignedOut>
+                    <LoggedOutLinks />
+                </SignedOut>
+            </div>
         </div>
      );
 }
