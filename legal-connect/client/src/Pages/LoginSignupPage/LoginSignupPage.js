1be175502b8f4a7c8eea05f719f5619214628f85
tripleyouuu <emailvitha@gmail.com>
2025-02-21 11:02:31 +0530

    removed unnecessary pages

--- legal-connect/client/src/Pages/LoginSignupPage/LoginSignupPage.js
+++
@@ -1,9 +0,0 @@
-import './LoginSignupPage.css';
-import { SignIn } from "@clerk/clerk-react";
-
-const LoginSignupPage = () => {
-
-  return <div className="login-signup-form"><SignIn/></div>;
-};
-
-export default LoginSignupPage;
